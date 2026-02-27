84a937a16e5d7ab84de0b8fb7989cb9d6db5abd8
cadelux <cadelux@github.com>
2026-02-27 04:25:09 +0300

    fix: sekme sirasi Yapacaklarim once, sallanan SVG hilal animasyonu

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,13 +30,24 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          {/* Islamic Crescent Decorations */}
-          <span className="crescent-decoration crescent-left-1" aria-hidden="true">☽</span>
-          <span className="crescent-decoration crescent-left-2" aria-hidden="true">☽</span>
-          <span className="crescent-decoration crescent-left-3" aria-hidden="true">☽</span>
-          <span className="crescent-decoration crescent-right-1" aria-hidden="true">☽</span>
-          <span className="crescent-decoration crescent-right-2" aria-hidden="true">☽</span>
-          <span className="crescent-decoration crescent-right-3" aria-hidden="true">☽</span>
+          {/* Islamic Crescent — hanging from top-left, swinging gently */}
+          <div className="crescent-hanger crescent-animate" aria-hidden="true">
+            <div className="crescent-string" />
+            <div className="crescent-icon">
+              <svg
+                width="26"
+                height="26"
+                viewBox="0 0 100 100"
+                xmlns="http://www.w3.org/2000/svg"
+                className="crescent-svg"
+              >
+                {/* Crescent moon path */}
+                <path
+                  d="M70 50C70 65.464 57.464 78 42 78C34.267 78 27.267 74.733 22 69.467C25.333 71.133 29.067 72 33 72C47.912 72 60 59.912 60 45C60 37.088 56.533 30.067 51 25.333C52.333 25.133 53.667 25 55 25C64.389 25 72.611 31.222 70 50Z"
+                />
+              </svg>
+            </div>
+          </div>
           {children}
         </AuthProvider>
       </body>
