7f4e6c35236a5b0aab6e063456fd711df752f8a6
cadelux <cadelux@github.com>
2026-02-27 04:30:56 +0300

    fix: .nojekyll ekle - GitHub Pages _next klasorunu goruncek, sallanan hilal temizlendi

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          {/* Islamic Crescent — hanging from top-left, swinging gently */}
           <div className="crescent-hanger crescent-animate" aria-hidden="true">
             <div className="crescent-string" />
             <div className="crescent-icon">
@@ -41,10 +40,7 @@ export default function RootLayout({
                 xmlns="http://www.w3.org/2000/svg"
                 className="crescent-svg"
               >
-                {/* Crescent moon path */}
-                <path
-                  d="M70 50C70 65.464 57.464 78 42 78C34.267 78 27.267 74.733 22 69.467C25.333 71.133 29.067 72 33 72C47.912 72 60 59.912 60 45C60 37.088 56.533 30.067 51 25.333C52.333 25.133 53.667 25 55 25C64.389 25 72.611 31.222 70 50Z"
-                />
+                <path d="M70 50C70 65.464 57.464 78 42 78C34.267 78 27.267 74.733 22 69.467C25.333 71.133 29.067 72 33 72C47.912 72 60 59.912 60 45C60 37.088 56.533 30.067 51 25.333C52.333 25.133 53.667 25 55 25C64.389 25 72.611 31.222 70 50Z" />
               </svg>
             </div>
           </div>
